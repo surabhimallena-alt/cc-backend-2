3ef4cdbaf06a9d6f0599245d2a5f60eae5a7d1ee
surabhimallena-alt <surabhi.mallena@gmail.com>
2025-09-04 15:42:15 +0530

    created json fiile with data, taken filename as first command line argument

--- task2.py
+++ task2.py
@@ -1,36 +1,54 @@
 import requests
 import json
+import sys
+from concurrent.futures import ThreadPoolExecutor
 
-pokemon = open("pokemon.txt", "r")
-data = {}
+def fetch_pokemon_data(name):
+    poke_url = f"https://pokeapi.co/api/v2/pokemon/{name}"
+    species_url = f"https://pokeapi.co/api/v2/pokemon-species/{name}"
 
-for line in pokemon:
-    poke_url = f"https://pokeapi.co/api/v2/pokemon/{line.strip()}"
-    species_url = f"https://pokeapi.co/api/v2/pokemon-species/{line.strip()}"
-    
     poke_response = requests.get(poke_url)
     species_response = requests.get(species_url)
 
     poke_data = poke_response.json()
     species_data = species_response.json()
 
-    poke_name = poke_data['name']
-    poke_id = poke_data['id']
-    poke_abilities = poke_data['abilities']
-    poke_type = poke_data['types']
-    is_legendary = species_data['is_legendary']
-    is_mythical = species_data['is_mythical']
+    return {
+        'name': poke_data['name'],
+        'id': poke_data['id'],
+        'abilities': poke_data['abilities'],
+        'type': poke_data['types'],
+        'is_legendary': species_data['is_legendary'],
+        'is_mythical': species_data['is_mythical']
+    }
 
-    poke_data = {
-        'id':poke_id, 
-        'abilities':poke_abilities, 
-        'type':poke_type, 
-        'is_legendary':is_legendary, 
-        'is_mythical':is_mythical
-        }
-    
-    data[poke_name] = poke_data
+data = {}
+with open("pokemon.txt") as f:
+    pokemon_names = [line.strip() for line in f]
 
-print(data)
+with ThreadPoolExecutor(max_workers=20) as executor:
+    results = executor.map(fetch_pokemon_data, pokemon_names)
 
-    
+for result in results:
+    data[result['name']] = {
+        'id': result['id'],
+        'abilities': result['abilities'],
+        'type': result['type'],
+        'is_legendary': result['is_legendary'],
+        'is_mythical': result['is_mythical']
+    }
+
+if len(sys.argv) < 2:
+    print("Usage: python write_json.py <filename.json>")
+    sys.exit(1)
+
+# Get the filename from the command line
+filename = sys.argv[1]
+
+# Optional: Add .json extension if not present
+if not filename.endswith(".json"):
+    filename += ".json"
+
+# Write the dictionary to the file (create or overwrite)
+with open(filename, "w") as f:
+    json.dump(data, f, indent=4)
