c3ee5dd17ba4033fbbd2e0e50b381aa5c055da19
surabhimallena-alt <surabhi.mallena@gmail.com>
2025-09-04 19:38:59 +0530

    damage_multiplier 2D array is created

--- task2B.py
+++ task2B.py
@@ -3,28 +3,50 @@ import json
 import sys
 from concurrent.futures import ThreadPoolExecutor
 
-def fetch_type_data(type_name):
-    type_url = f"https://pokeapi.co/api/v2/type/{type_name}"
-    type_response = requests.get(type_url)
-    type_data = type_response.json()
-    double_damage_from = [type_data['damage_relations']['double_damage_from'][i]['name'] \
-                          for i in range(len(type_data['damage_relations']['double_damage_from']))]
-    half_damage_from = [type_data['damage_relations']['half_damage_from'][i]['name'] \
-                          for i in range(len(type_data['damage_relations']['half_damage_from']))]
-    no_damage_from = [type_data['damage_relations']['no_damage_from'][i]['name'] \
-                          for i in range(len(type_data['damage_relations']['no_damage_from']))]
-    print(double_damage_from)
-    print(half_damage_from)
-    print(no_damage_from)
-
-fetch_type_data('normal')
-
 all_types_url = f"https://pokeapi.co/api/v2/type"
 all_types_response = requests.get(all_types_url)
 all_types_data = all_types_response.json()
 
 # dictionary with key:value as type_name:index_in_2D_list
 # index in 2D list is one less than the type ID
-type_dict = {all_types_data['results'][index]['name']:index for index in range(19)}
+type_dict = {all_types_data['results'][index]['name']:index for index in range(18)}
 
-damage_multipliers = [['1' for i in range(19)] for i in range(19)]
+damage_multipliers = [['1' for i in range(18)] for i in range(18)]
+
+def fetch_type_data(defender_name):
+    global damage_multipliers, type_dict
+
+    type_url = f"https://pokeapi.co/api/v2/type/{defender_name}"
+    type_response = requests.get(type_url)
+    type_data = type_response.json()
+
+    #list comprehension to create list of types that type_name deals damage from
+    double_damage_from = [type_data['damage_relations']['double_damage_from'][i]['name'] \
+                          for i in range(len(type_data['damage_relations']['double_damage_from']))]
+    half_damage_from = [type_data['damage_relations']['half_damage_from'][i]['name'] \
+                          for i in range(len(type_data['damage_relations']['half_damage_from']))]
+    no_damage_from = [type_data['damage_relations']['no_damage_from'][i]['name'] \
+                          for i in range(len(type_data['damage_relations']['no_damage_from']))]
+    
+    row_index = type_dict[defender_name]
+
+    for attacker_name in double_damage_from:
+        col_index = type_dict[attacker_name]
+        damage_multipliers[row_index][col_index] = '2'
+
+    for attacker_name in half_damage_from:
+        col_index = type_dict[attacker_name]
+        damage_multipliers[row_index][col_index] = '1/2'
+
+    for attacker_name in no_damage_from:
+        col_index = type_dict[attacker_name]
+        damage_multipliers[row_index][col_index] = '0'
+
+
+    
+
+with ThreadPoolExecutor(max_workers=5) as executor:
+    results = executor.map(fetch_type_data, type_dict.keys())
+
+for row in damage_multipliers:
+    print(row)
